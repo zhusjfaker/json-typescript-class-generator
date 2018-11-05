ba011053cdad490a6607e6de041bff1a1aefc072
椛船 <jayson.zsj@alibaba-inc.com>
2018-11-05 20:27:27 +0800

    zhusj

--- bin/index.js
+++ bin/index.js
@@ -8,6 +8,17 @@ const covert = (str) => {
         return letter.toUpperCase();
     });
 };
+const mkdirSyncRecursion = (dirname) => {
+    if (fs.existsSync(dirname)) {
+        return true;
+    }
+    else {
+        if (mkdirSyncRecursion(path.dirname(dirname))) {
+            fs.mkdirSync(dirname);
+            return true;
+        }
+    }
+};
 const generator = (argv) => {
     const fileSession = readline.createInterface({
         input: process.stdin,
@@ -27,7 +38,13 @@ const generator = (argv) => {
         return new Promise((resolve, reject) => {
             fileSession.question(`确认当前默认路径(${classespath}): \n`, (answer) => {
                 if (answer && answer != "") {
-                    classespath = path.resolve(__dirname) + answer;
+                    classespath = process.cwd() + "/" + answer;
+                    if (classespath[classespath.length - 1] != "/") {
+                        classespath = classespath + "/";
+                    }
+                }
+                if (!fs.existsSync(classespath)) {
+                    mkdirSyncRecursion(classespath);
                 }
                 resolve();
             });
@@ -42,10 +59,189 @@ const generator = (argv) => {
             });
         });
     };
+    const createContentClass = () => {
+        return new Promise((resolve, reject) => {
+            fileSession.question("您的转化对象JSON字符串位置(./covert.json): \n", (answer) => {
+                var jsonpath = process.cwd() + "/covert.json";
+                if (answer && answer != "") {
+                    jsonpath = process.cwd() + answer;
+                }
+                if (!fs.existsSync(jsonpath)) {
+                    console.log(`需要转化的json文件${jsonpath}不存在!程序异常暂停!`);
+                    resolve();
+                }
+                var data = fs.readFileSync(jsonpath, 'utf8');
+                console.log(data);
+                var originModel = JSON.parse(data);
+                var keylist = Object.getOwnPropertyNames(originModel);
+                console.log("当前键值数组:");
+                console.log(keylist);
+                let keycontent = "";
+                keylist.every((p) => {
+                    console.log("当前" + p + "的类型:" + typeof originModel[p]);
+                    if (typeof originModel[p] == "string") {
+                        keycontent += `
+
+    private _${p}!: string;
+
+    /**
+     * Getter ${p}
+     * @return {string}
+     */
+    @JsonProp()
+    public get ${p}(): string {
+        return this._${p};
+    }
+
+    /**
+     * Setter ${p}
+     * @param {string} value
+     */
+    public set ${p}(value: string) {
+        this._${p} = value;
+    }
+                        `;
+                    }
+                    else if (typeof originModel[p] == "number") {
+                        keycontent += `
+
+    private _${p}!: number;
+
+    /**
+     * Getter ${p}
+     * @return {number}
+     */
+    @JsonProp()
+    public get ${p}(): number {
+        return this._${p};
+    }
+
+    /**
+     * Setter ${p}
+     * @param {number} value
+     */
+    public set ${p}(value: number) {
+        this._${p} = value;
+    }
+                        `;
+                    }
+                    else if (typeof originModel[p] == "object") {
+                        if (originModel[p] === null) {
+                            keycontent += `
+
+    private _${p}?: any;
+
+    /**
+     * Getter ${p}
+     * @return {any}
+     */
+    @JsonProp()
+    public get ${p}(): any {
+        return this._${p}!;
+    }
+
+    /**
+     * Setter ${p}
+     * @param {any} value
+     */
+    public set ${p}(value: any) {
+        this._${p} = value;
+    }
+                                                `;
+                        }
+                        else if (Array.isArray(originModel[p])) {
+                            keycontent += `
+
+    private _${p}?: any[];
+
+    /**
+     * Getter ${p}
+     * @return {any[]}
+     */
+    @JsonProp()
+    public get ${p}(): any[] {
+        return this._${p}!;
+    }
+
+    /**
+     * Setter ${p}
+     * @param {any[]} value
+     */
+    public set ${p}(value: any[]) {
+        this._${p} = value;
+    }
+    `;
+                        }
+                        else {
+                            keycontent += `
+
+    private _${p}?: any;
+
+    /**
+     * Getter ${p}
+     * @return {any}
+     */
+    @JsonProp()
+    public get ${p}(): any {
+        return this._${p}!;
+    }
+
+    /**
+     * Setter ${p}
+     * @param {any} value
+     */
+    public set ${p}(value: any) {
+        this._${p} = value;
+    }
+                                                `;
+                        }
+                    }
+                    else if (typeof originModel[p] == "undefined") {
+                        keycontent += `
+
+    private _${p}?: any;
+
+    /**
+     * Getter ${p}
+     * @return {any}
+     */
+    @JsonProp()
+    public get ${p}(): any {
+        return this._${p}!;
+    }
+
+    /**
+     * Setter ${p}
+     * @param {any} value
+     */
+    public set ${p}(value: any) {
+        this._${p} = value;
+    }
+                        `;
+                    }
+                    return true;
+                });
+                /** 开始写文件 */
+                let content = `
+import { JsonSerializable, JsonProp } from 'jsonmodify';
+                
+export class ${filename} extends JsonSerializable{
+    
+    constructor() {
+        super();
+    }
+    ${keycontent}
+}
+`;
+                fs.writeFileSync(filepath, content, "utf8");
+                resolve();
+            });
+        });
+    };
     const session = async () => {
         await comfirmClassPath();
         await createFileSeesion();
-        // await createContentClass();
+        await createContentClass();
         fileSession.close();
     };
     session();
